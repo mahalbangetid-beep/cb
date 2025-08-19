61c7b36d93c15c8525ae9602fc691ead693338a7
mahalbangetid-beep <mahalbangetid@gmail.com>
2025-08-20 05:28:32 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,14 @@
+local modules = {}
+
+if _G.__CHESS_AI_LOADED__FULL then
+    warn("Script already loaded.")
+    return
+end
+_G.__CHESS_AI_LOADED__FULL = true
+
+modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/config.lua"))()
+modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/state.lua"))()
+modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/ai.lua"))()
+modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/gui.lua"))()
+
+modules.gui.init(modules)
